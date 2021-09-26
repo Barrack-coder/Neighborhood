3cbbfc353ee30e6bb001b4297a0ad48dda512be3
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-09-26 17:24:45 +0300

    add removed the secret key from setting.py

--- neighbor/settings.py
+++ neighbor/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@am=ppogqlxo6-&2zwbnvvs6w_2bt+-lyf8xh^n_mc0h9$9c(z'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
