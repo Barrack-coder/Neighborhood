3487a0ef484c8f12d0fd1d9360228ff7074842de
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-09-27 10:59:14 +0300

    defined authorities in views.py

--- hood/urls.py
+++ hood/urls.py
@@ -7,6 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     url(r'^$',views.index,name='Index'),
+    url(r'^authorities',views.authorities, name='authorities'),
 ] 
 
 
--- hood/views.py
+++ hood/views.py
@@ -3,4 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,'index.html')
+    return render(request,'index.html',)
+
+def authorities(request):
+    return render(request,'authorities/authorities.html',)
