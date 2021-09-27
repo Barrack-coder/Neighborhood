0cb62fed1128d3d4c15769bdc16b08f76e21efbe
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-09-27 11:43:08 +0300

    modified blogpost

--- hood/urls.py
+++ hood/urls.py
@@ -8,6 +8,9 @@ from django.conf.urls.static import static
 urlpatterns=[
     url(r'^$',views.index,name='Index'),
     url(r'^authorities',views.authorities, name='authorities'),
+    url(r'^blog',views.blog, name='blog'),
+    url(r'^new/blogpost$',views.new_blogpost, name='new-blogpost'), 
+    url(r'^view/blog/(\d+)',views.view_blog,name='view_blog'),
 ] 
 
 
--- hood/views.py
+++ hood/views.py
@@ -7,3 +7,18 @@ def index(request):
 
 def authorities(request):
     return render(request,'authorities/authorities.html',)
+
+
+def new_blogpost(request):
+    return render(request,'blog/blogpost_form.html',)
+
+def blog(request):
+    return render(request,'blog/blogs.html',)
+
+
+def view_blog(request):
+    return render(request,'blog/view_blog.html',)
+
+
+
+
