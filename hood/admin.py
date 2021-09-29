b01ffedd4f0c72b94ce7ea31e27042d3b1ac724c
Barrack-coder <barrack.juma@student.moringaschool.com>
2021-09-29 05:05:33 +0300

    registered sites in admin.py

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,12 @@
+from hood.models import Authorities, BlogPost, Business, Health, Profile, healthservices, neighbourhood, notifications
 from django.contrib import admin
 
 # Register your models here.
+admin.site.register(Profile);
+admin.site.register(neighbourhood);
+admin.site.register(BlogPost);
+admin.site.register(Business);
+admin.site.register(Health);
+admin.site.register(healthservices);
+admin.site.register(Authorities);
+admin.site.register(notifications);
